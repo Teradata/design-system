f41e98b8ee29bbe9d3128cf45af02193e6a2238b
Vega <obed.vega@teradata.com>
2024-12-10 15:15:43 -0800

    feat(eyebrow): adding eyebrow variation variable

--- libs/react-components/src/lib/components/Typography/index.tsx
+++ libs/react-components/src/lib/components/Typography/index.tsx
@@ -13,18 +13,17 @@ interface TypographyProps {
     | 'headline2'
     | 'headline3'
     | 'caption'
-    | 'eyebrow';
-  alignment: 'center' | 'left' | 'right';  
+    | 'eyebrow'
+    | 'eyebrow2';
   children?: ReactNode;
 }
 
 const Typography: React.FC<TypographyProps> = ({
   scale = 'body1',
-  alignment = 'center',
   children,
 }) => {
   return (
-    <slot className={`${styles[scale]} ${alignment && scale === 'eyebrow' ? styles[alignment] : 'center'} ${styles.typography}`}>{children}</slot>
+    <slot className={`${styles[scale]} ${styles.typography}`}>{children}</slot>
   );
 };
 
